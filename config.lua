c78a01195f49342d3c6b18e5e1de553b641bd82b
Lubeck Huaman <lubeck05@gmail.com>
2024-02-04 01:18:46 -0500

    useless lines

--- config.lua
+++ config.lua
@@ -1,21 +1,30 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
---
-
--- lvim.colorscheme = "dracula"
--- lvim.format_on_save.enabled = true
--- lvim.keys.normal_mode["<C-h>"] = ":!echo 11<CR>"
-
-
--- lvim.transparent_window = true
--- vim.opt.background = 'light'
 vim.lsp.set_log_level("trace")
 lvim.log.level = "info"
 
 lvim.plugins = {
   "shaunsingh/solarized.nvim",
+  -- {
+  --   "maxmx03/solarized.nvim",
+  --   config = function()
+  --     require('solarized').setup({
+  --       theme = 'neo',
+  --       colors = function(colors, colorhelper)
+  --         local darken = colorhelper.darken
+  --         local lighten = colorhelper.lighten
+  --         local blend = colorhelper.blend
+  --         return {
+  --           fg = '#d3d', -- output: #ffffff
+  --           bg = lighten(colors.base03, 7)
+  --         }
+  --       end,
+  --       highlights = function(colors)
+  --         return {
+  --           Normal = { fg = colors.fg, bg = colors.bg }
+  --         }
+  --       end
+  --     })
+  --   end
+  -- },
   "folke/trouble.nvim",
   "Mofiqul/dracula.nvim",
   "ibhagwan/fzf-lua",
@@ -52,7 +61,9 @@ lvim.plugins = {
   "morhetz/gruvbox",
   "sainnhe/gruvbox-material",
   "luisiacc/gruvbox-baby",
-  "ray-x/starry.nvim"
+  "catppuccin/nvim",
+  "mhartington/oceanic-next",
+  "cpea2506/one_monokai.nvim"
 }
 
 
@@ -61,51 +72,23 @@ vim.g.codedark_modern = false
 -- vim.g.codedark_transparent = true
 
 -- disable default jdtls ---
--- vim.g.starry_italic_comments = true
--- vim.o.bg = "light"
 
 -- vim.g.solarized_italic_comments = true
 -- vim.g.solarized_italic_keywords = true
 -- vim.g.solarized_italic_functions = true
 -- vim.g.solarized_italic_variables = true
-vim.g.solarized_contrast = true
+-- vim.g.solarized_contrast = true
 vim.g.solarized_borders = true
 
-
 lvim.colorscheme = "solarized"
--- lvim.colorscheme = "monokai-pro"
--- lvim.colorscheme = "codedark"
--- lvim.colorscheme = "seoul256"
--- lvim.colorscheme = "kanagawa"
--- lvim.colorscheme = "melange"
--- lvim.colorscheme = "everforest"
--- lvim.colorscheme = "visual_studio_code"
-
--- lvim.colorscheme = "material"
--- vim.g.material_style = "palenight"
-
--- lvim.colorscheme = "evergarden"
--- lvim.colorscheme = "nvcode"
-
 
 -- vim.g.gruvbox_contrast_dark = "medium"
 -- vim.g.gruvbox_italicize_comments = true
 -- vim.g.gruvbox_contrast_dark = "soft"
 -- vim.g.gruvbox_invert_tabline = true
--- lvim.colorscheme = "gruvbox"
-
--- lvim.colorscheme = "gruvbox-material"
--- lvim.colorscheme = "gruvbox-baby"
--- lvim.colorscheme = "oceanic"
-
--- lvim.colorscheme = "dracula-soft"
 
 vim.lsp.diagnostic.show_line_diagnostics = true -- * Use |vim.diagnostic.open_float()| instead.
 vim.lsp.diagnostic.show_position_diagnostics = true
-
-
--- map <space>e :lua vim.diagnostic.open_float(0, {scope="line"})<CR>
--- lvim.keys.normal_mode['<S-t>'] = "<Cmd>echo Okay!<CR>"
 
 lvim.builtin.which_key.mappings["E"] = {
   "<Cmd>echo Okay!<CR>"
@@ -138,21 +121,16 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
   group = "lsp_diagnostics_hold",
 })
 
-
--- set numberwidth = 5
-
 vim.opt.signcolumn = "yes"
-
 vim.opt.statuscolumn = " %C%s%l"
 vim.opt.numberwidth = 7
 
-
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
--- lvim.format_on_save = true -- Disable this line
 lvim.format_on_save.enabled = true
--- lvim.format_on_save.pattern = { "*.lua", "*.py" }
 
--- vim.g.bac
--- vim.opt.background = 'light'
--- vim.opt.background = "light"
+require('indent_blankline').setup {
+  scope = {
+    show_start = false
+  }
+}
